6a777044cbc4a82025ad5124969e978332fd511c
ThePhD <phdoftheouse@gmail.com>
2015-06-12 05:25:47 -0400

    Updated jsonpp and preparing for parser of hlsl preprocessor

--- hlsl/preprocessor/parser.hpp
+++ hlsl/preprocessor/parser.hpp
@@ -10,14 +10,54 @@
 
 namespace gld { namespace hlsl { namespace preprocessor {
 
-	struct root {
+	struct sequence {
+		buffer_view<token> tokens;
+	};
 
+	struct symbol : sequence {
+		std::reference_wrapper<token> name;
+	};
+
+	struct definition : symbol {
+		std::vector<std::reference_wrapper<token>> parameters;
+		variant<unit,
+			symbol,
+			string_view,
+			bool,
+			uint8, uint16, uint32, uint64,
+			int8, int16, int32, int64,
+			code_point,
+			half, float, double> value;
+	};
+
+	struct expression : sequence {
+
+	};
+
+	struct conditional : sequence {
+		std::vector<std::reference_wrapper<token>> expression;
+	};
+
+	struct inclusion : sequence {
+
+	};
+	
+	struct scope {
+		conditional condition;
+		std::vector<definition> defines;
+		std::vector<inclusion> includes;
+		std::vector<definition> defines;
+	};
+
+	struct top_level {
+		string data;
+		std::vector<scope> scopes;
 	};
 
 	class parser {
 	private:
 		buffer_view<token> tokens;
-
+		top_level toplevel;
 
 	public:
 		parser( buffer_view<token> tokenview ) : tokens( std::move( tokenview ) ) {
